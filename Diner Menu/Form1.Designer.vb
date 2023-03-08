129cf0a3e31e874dfbc062ef8c48bcc309c26ab5
bbre1666 <badenbrenner@isu.edu>
2023-03-08 08:23:22 -0700

    Add project files.

---
+++ Diner Menu/Form1.Designer.vb
@@ -0,0 +1,246 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class DinerMenuForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.SoupButton = New System.Windows.Forms.Button()
+        Me.SaladButton = New System.Windows.Forms.Button()
+        Me.FishButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.MainMenueGroupBox = New System.Windows.Forms.GroupBox()
+        Me.SoupGroupBox = New System.Windows.Forms.GroupBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.FishGroupBox = New System.Windows.Forms.GroupBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SaladGroupBox = New System.Windows.Forms.GroupBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.MainMenueGroupBox.SuspendLayout()
+        Me.SoupGroupBox.SuspendLayout()
+        Me.FishGroupBox.SuspendLayout()
+        Me.SaladGroupBox.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SoupButton
+        '
+        Me.SoupButton.Location = New System.Drawing.Point(300, 540)
+        Me.SoupButton.Name = "SoupButton"
+        Me.SoupButton.Size = New System.Drawing.Size(129, 64)
+        Me.SoupButton.TabIndex = 0
+        Me.SoupButton.Text = "Soup"
+        Me.SoupButton.UseVisualStyleBackColor = True
+        '
+        'SaladButton
+        '
+        Me.SaladButton.Location = New System.Drawing.Point(435, 540)
+        Me.SaladButton.Name = "SaladButton"
+        Me.SaladButton.Size = New System.Drawing.Size(129, 64)
+        Me.SaladButton.TabIndex = 1
+        Me.SaladButton.Text = "Salad"
+        Me.SaladButton.UseVisualStyleBackColor = True
+        '
+        'FishButton
+        '
+        Me.FishButton.Location = New System.Drawing.Point(300, 608)
+        Me.FishButton.Name = "FishButton"
+        Me.FishButton.Size = New System.Drawing.Size(129, 64)
+        Me.FishButton.TabIndex = 2
+        Me.FishButton.Text = "Fish"
+        Me.FishButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(435, 608)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(129, 64)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Palace Script MT", 48.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(264, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(349, 74)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "The Snake Pit"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(379, 105)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(107, 20)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Todays Special"
+        '
+        'MainMenueGroupBox
+        '
+        Me.MainMenueGroupBox.Controls.Add(Me.Label2)
+        Me.MainMenueGroupBox.Controls.Add(Me.Label1)
+        Me.MainMenueGroupBox.Location = New System.Drawing.Point(12, 12)
+        Me.MainMenueGroupBox.Name = "MainMenueGroupBox"
+        Me.MainMenueGroupBox.Size = New System.Drawing.Size(872, 167)
+        Me.MainMenueGroupBox.TabIndex = 6
+        Me.MainMenueGroupBox.TabStop = False
+        '
+        'SoupGroupBox
+        '
+        Me.SoupGroupBox.Controls.Add(Me.Label6)
+        Me.SoupGroupBox.Controls.Add(Me.Label3)
+        Me.SoupGroupBox.Location = New System.Drawing.Point(12, 185)
+        Me.SoupGroupBox.Name = "SoupGroupBox"
+        Me.SoupGroupBox.Size = New System.Drawing.Size(875, 349)
+        Me.SoupGroupBox.TabIndex = 7
+        Me.SoupGroupBox.TabStop = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(6, 132)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(862, 20)
+        Me.Label6.TabIndex = 1
+        Me.Label6.Text = "The soup of the day is a hearty salmon chowder made with wild caught Alaskan coho" &
+    " salmon and are house made chowder base "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Vladimir Script", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(306, 56)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(246, 36)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "~Soup Of The Day~"
+        '
+        'FishGroupBox
+        '
+        Me.FishGroupBox.Controls.Add(Me.Label8)
+        Me.FishGroupBox.Controls.Add(Me.Label4)
+        Me.FishGroupBox.Location = New System.Drawing.Point(12, 185)
+        Me.FishGroupBox.Name = "FishGroupBox"
+        Me.FishGroupBox.Size = New System.Drawing.Size(875, 349)
+        Me.FishGroupBox.TabIndex = 8
+        Me.FishGroupBox.TabStop = False
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(178, 190)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(502, 20)
+        Me.Label8.TabIndex = 1
+        Me.Label8.Text = "The fish of the day is a marinaded sablefish served with rice and asparagus"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Vladimir Script", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(299, 80)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(233, 36)
+        Me.Label4.TabIndex = 0
+        Me.Label4.Text = "~Fish of The Day~"
+        '
+        'SaladGroupBox
+        '
+        Me.SaladGroupBox.Controls.Add(Me.Label7)
+        Me.SaladGroupBox.Controls.Add(Me.Label5)
+        Me.SaladGroupBox.Location = New System.Drawing.Point(12, 185)
+        Me.SaladGroupBox.Name = "SaladGroupBox"
+        Me.SaladGroupBox.Size = New System.Drawing.Size(875, 349)
+        Me.SaladGroupBox.TabIndex = 9
+        Me.SaladGroupBox.TabStop = False
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(33, 170)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(798, 20)
+        Me.Label7.TabIndex = 1
+        Me.Label7.Text = "The salad of the day is a rattle snake Caesar salad with pan fried rattle snake a" &
+    "nd our house made Caesar salad dressing"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Vladimir Script", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label5.Location = New System.Drawing.Point(299, 70)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(253, 36)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "~Salad Of The Day~"
+        '
+        'DinerMenuForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(896, 684)
+        Me.Controls.Add(Me.SaladGroupBox)
+        Me.Controls.Add(Me.FishGroupBox)
+        Me.Controls.Add(Me.SoupGroupBox)
+        Me.Controls.Add(Me.MainMenueGroupBox)
+        Me.Controls.Add(Me.FishButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.SoupButton)
+        Me.Controls.Add(Me.SaladButton)
+        Me.Name = "DinerMenuForm"
+        Me.Text = "Diner Menu"
+        Me.MainMenueGroupBox.ResumeLayout(False)
+        Me.MainMenueGroupBox.PerformLayout()
+        Me.SoupGroupBox.ResumeLayout(False)
+        Me.SoupGroupBox.PerformLayout()
+        Me.FishGroupBox.ResumeLayout(False)
+        Me.FishGroupBox.PerformLayout()
+        Me.SaladGroupBox.ResumeLayout(False)
+        Me.SaladGroupBox.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+
+
+    Friend WithEvents SoupButton As Button
+    Friend WithEvents SaladButton As Button
+    Friend WithEvents FishButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents MainMenueGroupBox As GroupBox
+    Friend WithEvents SoupGroupBox As GroupBox
+    Friend WithEvents FishGroupBox As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents SaladGroupBox As GroupBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+End Class
---
+++ Diner Menu/Form1.vb
@@ -0,0 +1,42 @@
+﻿Option Explicit On
+Option Strict On
+
+Public Class DinerMenuForm
+    Private Sub DinerMenuForm_Activated(sender As Object, e As EventArgs) Handles Me.Activated
+        SoupGroupBox.Hide()
+        SaladGroupBox.Hide()
+        FishGroupBox.Hide()
+
+
+    End Sub
+
+
+
+
+
+
+
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub FishButton_Click(sender As Object, e As EventArgs) Handles FishButton.Click
+        SoupGroupBox.Hide()
+        SaladGroupBox.Hide()
+        FishGroupBox.Show()
+
+    End Sub
+
+    Private Sub SoupButton_Click(sender As Object, e As EventArgs) Handles SoupButton.Click
+        FishGroupBox.Hide()
+        SaladGroupBox.Hide()
+        SoupGroupBox.Show()
+    End Sub
+
+    Private Sub SaladButton_Click(sender As Object, e As EventArgs) Handles SaladButton.Click
+        FishGroupBox.Hide()
+        SoupGroupBox.Hide()
+        SaladGroupBox.Show()
+    End Sub
+End Class
